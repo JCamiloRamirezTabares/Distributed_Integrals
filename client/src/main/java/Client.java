cc69f78509d5e36b2ee4ad1497c72ccc19c4e56e
santiagoprado21 <santiagoprado435@gmail.com>
2024-05-27 15:20:46 -0500

    Feature: Client - Agregar verificacion de que el limite inferior sea menor que el superior

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,15 +1,13 @@
 import model.Integral;
-import net.objecthunter.exp4j.tokenizer.UnknownFunctionOrVariableException;
 import ui.UIHandler;
 
 /*
     Tareas por hacer:
     Maneje las funciones: str -> Function                                   ==> done
-    Guardar informacion (Fucncion, limites de integracion)                  ==> done
-    Funcion que se encargue de pedir y guardar limites de integracion       ==> done
-    Implementar metricas para el cliente    
+    Guardar información (Función, límites de integración)                  ==> done
+    Función que se encargue de pedir y guardar límites de integración       ==> done
+    Implementar métricas para el cliente
  */
-
 
 public class Client {
 
@@ -17,7 +15,6 @@ public class Client {
         Esta es la controladora de la UI por consola
      */
     private static UIHandler handlerUI;
-
 
     public static void main(String[] args) {
         initialize();
@@ -29,67 +26,65 @@ public class Client {
             String lowerRange = "";
             String upperRange = "";
 
-            if(!input.equalsIgnoreCase("exit")){
-                lowerRange = handlerUI.lowerRangeMenu();
-                upperRange = handlerUI.upperRangeMenu();
+            if (!input.equalsIgnoreCase("exit")) {
+                boolean validRanges = false;
 
+                while (!validRanges) {
+                    lowerRange = handlerUI.lowerRangeMenu();
+                    upperRange = handlerUI.upperRangeMenu();
+
+                    try {
+                        double lower = Double.parseDouble(lowerRange);
+                        double upper = Double.parseDouble(upperRange);
+
+                        if (lower >= upper) {
+                            System.out.println("El límite inferior debe ser menor que el límite superior. Inténtelo de nuevo.");
+                        } else {
+                            validRanges = true;
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Por favor, introduzca un número válido.");
+                    }
+                }
 
                 Integral integral = buildIntegral(input, lowerRange, upperRange);
 
-                // Desde aqui
+                // Desde aquí
                 MonteCarlo monteCarlo = new MonteCarlo();
-                
-                try{
-                    double res = monteCarlo.solve(integral);
-                    System.out.println(""
-                        +"|| \n"
-                        +"|| La integral "+integral.toString()+" es aproximadamente: " + res
-                    +"");
 
-                    String filename = "resultados_integrales.txt";
-                    monteCarlo.saveResultsToFile(filename, integral, res);
-
-                } catch(UnknownFunctionOrVariableException e){
-                    System.out.println(""
-                    +"|| \n"
-                    +"|| Ha ocurrido un error, porfavor intentalo de nuevo \n"
-                    +"|| Asegurate de ingresar una funcion matematica valida"
-                    +"");
-                }
+                double res = monteCarlo.solve(integral);
+                System.out.println(""+
+                    "|| La integral " + integral.toString() + " es aproximadamente: " + res
+                + "");
                 
+                // Guardar el resultado en un archivo .txt en la carpeta docs
+                String filename = "resultados_integrales.txt";
+                monteCarlo.saveResultsToFile(filename, integral, res);
                 
-                //Hasta aqui, es un set de codigo que debe ser cambiado para distribuido
+                // Hasta aquí, es un set de código que debe ser cambiado para distribuido
 
-            } else{
+            } else {
                 sentinel = false;
                 handlerUI.byebye();
             }
         }
-
     }
 
     /*
         Inicializa lo necesario para que trabaje el cliente (Es como el constructor)
      */
-    private static void initialize(){
+    private static void initialize() {
         handlerUI = new UIHandler();
     }
 
     /*
         Se encarga de construir la integral
      */
-    private static Integral buildIntegral(String funct, String lwrRange, String upprRange){
-
+    private static Integral buildIntegral(String funct, String lwrRange, String upprRange) {
         String function = funct;
         Double lowerRange = Double.parseDouble(lwrRange);
         Double upperRange = Double.parseDouble(upprRange);
 
-
         return new Integral(function, lowerRange, upperRange);
     }
-    
-    
-
-
-
 }
